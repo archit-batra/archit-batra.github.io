35732579383c7313def540b1c809ecdaf40bc974
archit-batra <rapchit93@gmail.com>
2021-01-04 20:32:04 +0530

    design changes

--- sw.js
+++ sw.js
@@ -18,7 +18,28 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('your-magic-cache').then(function(cache) {
       return cache.addAll([
-        '/'
+        '/index.html',
+		'/assets/css/style.css',
+		'/assets/img/favicon.png',
+		'/assets/img/apple-touch-icon.png',
+		'/assets/vendor/bootstrap/css/bootstrap.min.css',
+		'/assets/vendor/aos/aos.css',
+		'/assets/css/style.css',
+		'/assets/vendor/jquery/jquery.min.js',
+		'/assets/vendor/jquery.easing/jquery.easing.min.js',
+		'/assets/vendor/waypoints/jquery.waypoints.min.js',
+		'/assets/vendor/counterup/counterup.min.js',
+		'/assets/vendor/typed.js/typed.min.js',
+		'/assets/vendor/aos/aos.js',
+		'/site.js',
+		'/assets/js/main.js',
+		'/sw.js',
+		'/assets/img/profile-img.webp',
+		'/assets/img/testimonials/testimonials-1.webp',
+		'/assets/img/hero-bg.webp',
+		'/icon-192.png',
+		'/icon-512.png',
+		'/manifest.json'
       ]);
     })
   );
@@ -28,7 +49,7 @@ self.addEventListener('fetch', function(event) {
   if (event.request.url == 'https://batraarchit.com/') {
     console.info('responding to server fetch with Service Worker!');
     event.respondWith(fetch(event.request).catch(function(e) {
-      let out = {Gold: 1, Size: -1, Actions: []};
+      let out = {Oops! Looks like you're offline.};
       return new Response(JSON.stringify(out));
     }));
     return;
